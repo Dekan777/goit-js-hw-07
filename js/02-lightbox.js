b258fcf86424cad90c18e488aa6e90d388a8d3e0
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-10-31 11:44:36 +0200

    add
    simple-lightbox.min.css
    simple-lightbox.min.css

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" 
+                    />
+                </a>
+            </li>`;
+};
+
+const galleryCreate = galleryItems.map(createGalleryItem).join("");
+gallery.insertAdjacentHTML("beforeend", galleryCreate);
+
