959eb2a1f215727e3a18678d28201f99b271159b
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-10-31 16:55:11 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,11 +12,38 @@ const createGalleryItem = ({ preview, original, description }) => {
                         class="gallery__image" 
                         src="${preview}" 
                         alt="${description}" 
+                        title="${description}"
                     />
                 </a>
             </li>`;
 };
 
+
+
 const galleryCreate = galleryItems.map(createGalleryItem).join("");
 gallery.insertAdjacentHTML("beforeend", galleryCreate);
 
+document.addEventListener("click", (event) => {
+    const selectedElement = event.target.closest(".gallery__image");
+  
+   
+    if (selectedElement) {
+      event.preventDefault();
+
+     const lightbox = new SimpleLightbox('.gallery a', { 
+        captionPosition: 'bottom',
+        captionDelay: 250,
+       
+    });
+    
+    }
+  
+  });
+
+
+
+
+
+//   new SimpleLightbox('.gallery__image a', { /* options */ });
+
+//   $('.gallery__image a').simpleLightbox({ /* options */ });
